7bcd9d0ec92fe9b6816b71a85da93101c09cf02f
Elena Leusik <elena.leusik@gmail.com>
2023-04-26 10:42:58 +0200

    feat: add obj with Cyrillic key codes

--- js/langs.js
+++ js/langs.js
@@ -1,385 +1,515 @@
 const en = [[{
   value: '`',
   shiftValue: '~',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '1',
   shiftValue: '!',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '2',
   shiftValue: '@',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '3',
   shiftValue: '#',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '4',
   shiftValue: '$',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '5',
   shiftValue: '%',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '6',
   shiftValue: '^',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '7',
   shiftValue: '&',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '8',
   shiftValue: '*',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '9',
   shiftValue: '(',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '0',
   shiftValue: ')',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '-',
   shiftValue: '_',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '=',
   shiftValue: '+',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'Backspace',
   shiftValue: 'Backspace',
-  comand: [],
-  symbolic: false,
   class: 'keyboard__btn keyboard__btn_comand_big',
 }], [{
   value: 'Tab',
   shiftValue: 'Tab',
-  comand: [],
-  symbolic: false,
   class: 'keyboard__btn keyboard__btn_comand_small',
 }, {
   value: 'q',
   shiftValue: 'Q',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'w',
   shiftValue: 'W',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'e',
   shiftValue: 'E',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'r',
   shiftValue: 'R',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 't',
   shiftValue: 'T',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'y',
   shiftValue: 'Y',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'u',
   shiftValue: 'U',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'i',
   shiftValue: 'I',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'o',
   shiftValue: 'O',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'p',
   shiftValue: 'P',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '[',
   shiftValue: '{',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: ']',
   shiftValue: '}',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '\\',
   shiftValue: '|',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'Del',
   shiftValue: 'Del',
-  comand: [],
-  symbolic: false,
   class: 'keyboard__btn keyboard__btn_comand_small',
 }], [{
   value: 'CapsLock',
   shiftValue: 'CapsLock',
-  comand: [],
-  symbolic: false,
   class: 'keyboard__btn keyboard__btn_comand_big',
 }, {
   value: 'a',
   shiftValue: 'A',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 's',
   shiftValue: 'S',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'd',
   shiftValue: 'D',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'f',
   shiftValue: 'F',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'g',
   shiftValue: 'G',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'h',
   shiftValue: 'H',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'j',
   shiftValue: 'J',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'k',
   shiftValue: 'K',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'l',
   shiftValue: 'L',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: ';',
   shiftValue: ':',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '\'',
   shiftValue: '"',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'Enter',
   shiftValue: 'Enter',
-  comand: [],
-  symbolic: false,
   class: 'keyboard__btn keyboard__btn_comand_big',
 }], [{
   value: 'Shift',
   shiftValue: 'Shift',
-  comand: [],
-  symbolic: false,
   class: 'keyboard__btn keyboard__btn_comand_big',
 }, {
   value: 'z',
   shiftValue: 'Z',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'x',
   shiftValue: 'X',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'c',
   shiftValue: 'C',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'v',
   shiftValue: 'V',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'b',
   shiftValue: 'B',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'n',
   shiftValue: 'N',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'm',
   shiftValue: 'M',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: ',',
   shiftValue: '<',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '.',
   shiftValue: '>',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '/',
   shiftValue: '?',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '↑',
   shiftValue: '↑',
-  comand: [],
-  symbolic: false,
   class: 'keyboard__btn keyboard__btn_comand_small',
 }, {
   value: 'Shift',
   shiftValue: 'Shift',
-  comand: [],
-  symbolic: false,
   class: 'keyboard__btn keyboard__btn_comand_big',
 }], [{
   value: 'Ctrl',
   shiftValue: 'Ctrl',
-  comand: [],
-  symbolic: false,
   class: 'keyboard__btn keyboard__btn_comand_small',
 }, {
   value: 'Win',
   shiftValue: 'Win',
-  comand: [],
-  symbolic: false,
   class: 'keyboard__btn keyboard__btn_comand_small',
 }, {
   value: 'Alt',
   shiftValue: 'Alt',
-  comand: [],
-  symbolic: false,
   class: 'keyboard__btn keyboard__btn_comand_small',
 }, {
   value: ' ',
   shiftValue: ' ',
-  comand: [],
-  symbolic: true,
   class: 'keyboard__btn keyboard__btn_space',
 }, {
   value: 'Alt',
   shiftValue: 'Alt',
-  comand: [],
-  symbolic: false,
   class: 'keyboard__btn keyboard__btn_comand_small',
 }, {
   value: '←',
   shiftValue: '←',
-  comand: [],
-  symbolic: false,
   class: 'keyboard__btn keyboard__btn_comand_small',
 }, {
   value: '↓',
   shiftValue: '↓',
-  comand: [],
-  symbolic: false,
   class: 'keyboard__btn keyboard__btn_comand_small',
 }, {
   value: '→',
   shiftValue: '→',
-  comand: [],
-  symbolic: false,
   class: 'keyboard__btn keyboard__btn_comand_small',
 }, {
   value: 'Ctrl',
   shiftValue: 'Ctrl',
-  comand: [],
-  symbolic: false,
+  class: 'keyboard__btn keyboard__btn_comand_small',
+}]];
+
+const ru = [[{
+  value: 'ё',
+  shiftValue: 'Ё',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: '1',
+  shiftValue: '!',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: '2',
+  shiftValue: '"',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: '3',
+  shiftValue: '№',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: '4',
+  shiftValue: ';',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: '5',
+  shiftValue: '%',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: '6',
+  shiftValue: ':',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: '7',
+  shiftValue: '?',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: '8',
+  shiftValue: '*',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: '9',
+  shiftValue: '(',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: '0',
+  shiftValue: ')',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: '-',
+  shiftValue: '_',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: '=',
+  shiftValue: '+',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: 'Backspace',
+  shiftValue: 'Backspace',
+  class: 'keyboard__btn keyboard__btn_comand_big',
+}], [{
+  value: 'Tab',
+  shiftValue: 'Tab',
+  class: 'keyboard__btn keyboard__btn_comand_small',
+}, {
+  value: 'й',
+  shiftValue: 'Й',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: 'ц',
+  shiftValue: 'Ц',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: 'у',
+  shiftValue: 'У',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: 'к',
+  shiftValue: 'К',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: 'е',
+  shiftValue: 'Е',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: 'н',
+  shiftValue: 'Н',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: 'г',
+  shiftValue: 'Г',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: 'ш',
+  shiftValue: 'Ш',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: 'щ',
+  shiftValue: 'Щ',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: 'з',
+  shiftValue: 'З',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: 'х',
+  shiftValue: 'Х',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: 'ъ',
+  shiftValue: 'Ъ',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: '\\',
+  shiftValue: '/',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: 'Del',
+  shiftValue: 'Del',
+  class: 'keyboard__btn keyboard__btn_comand_small',
+}], [{
+  value: 'CapsLock',
+  shiftValue: 'CapsLock',
+  class: 'keyboard__btn keyboard__btn_comand_big',
+}, {
+  value: 'ф',
+  shiftValue: 'Ф',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: 'ы',
+  shiftValue: 'Ы',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: 'в',
+  shiftValue: 'В',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: 'а',
+  shiftValue: 'А',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: 'п',
+  shiftValue: 'П',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: 'р',
+  shiftValue: 'Р',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: 'о',
+  shiftValue: 'О',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: 'л',
+  shiftValue: 'Л',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: 'д',
+  shiftValue: 'Д',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: 'ж',
+  shiftValue: 'Ж',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: 'э',
+  shiftValue: 'Э',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: 'Enter',
+  shiftValue: 'Enter',
+  class: 'keyboard__btn keyboard__btn_comand_big',
+}], [{
+  value: 'Shift',
+  shiftValue: 'Shift',
+  class: 'keyboard__btn keyboard__btn_comand_big',
+}, {
+  value: 'я',
+  shiftValue: 'Я',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: 'ч',
+  shiftValue: 'Ч',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: 'c',
+  shiftValue: 'С',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: 'м',
+  shiftValue: 'М',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: 'и',
+  shiftValue: 'И',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: 'т',
+  shiftValue: 'Т',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: 'ь',
+  shiftValue: 'Ь',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: 'б',
+  shiftValue: 'Б',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: 'ю',
+  shiftValue: 'Ю',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: '.',
+  shiftValue: ',',
+  class: 'keyboard__btn keyboard__btn_symbol',
+}, {
+  value: '↑',
+  shiftValue: '↑',
+  class: 'keyboard__btn keyboard__btn_comand_small',
+}, {
+  value: 'Shift',
+  shiftValue: 'Shift',
+  class: 'keyboard__btn keyboard__btn_comand_big',
+}], [{
+  value: 'Ctrl',
+  shiftValue: 'Ctrl',
+  class: 'keyboard__btn keyboard__btn_comand_small',
+}, {
+  value: 'Win',
+  shiftValue: 'Win',
+  class: 'keyboard__btn keyboard__btn_comand_small',
+}, {
+  value: 'Alt',
+  shiftValue: 'Alt',
+  class: 'keyboard__btn keyboard__btn_comand_small',
+}, {
+  value: ' ',
+  shiftValue: ' ',
+  class: 'keyboard__btn keyboard__btn_space',
+}, {
+  value: 'Alt',
+  shiftValue: 'Alt',
+  class: 'keyboard__btn keyboard__btn_comand_small',
+}, {
+  value: '←',
+  shiftValue: '←',
+  class: 'keyboard__btn keyboard__btn_comand_small',
+}, {
+  value: '↓',
+  shiftValue: '↓',
+  class: 'keyboard__btn keyboard__btn_comand_small',
+}, {
+  value: '→',
+  shiftValue: '→',
+  class: 'keyboard__btn keyboard__btn_comand_small',
+}, {
+  value: 'Ctrl',
+  shiftValue: 'Ctrl',
   class: 'keyboard__btn keyboard__btn_comand_small',
 }]];
