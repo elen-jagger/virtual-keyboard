7db80a5d74d2981aa002ad1ca3758934d98b537c
Elena Leusik <elena.leusik@gmail.com>
2023-04-27 14:08:41 +0200

    feat: update data-for-render object with key.codes

--- js/langs.js
+++ js/langs.js
@@ -1,110 +1,137 @@
 const en = [[{
   value: '`',
   shiftValue: '~',
+  keycode: 'backquote',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '1',
   shiftValue: '!',
+  keycode: 'digit1',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '2',
   shiftValue: '@',
+  keycode: 'digit2',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '3',
   shiftValue: '#',
+  keycode: 'digit3',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '4',
   shiftValue: '$',
+  keycode: 'digit4',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '5',
   shiftValue: '%',
+  keycode: 'digit5',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '6',
   shiftValue: '^',
+  keycode: 'digit6',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '7',
   shiftValue: '&',
+  keycode: 'digit7',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '8',
   shiftValue: '*',
+  keycode: 'digit8',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '9',
   shiftValue: '(',
+  keycode: 'digit9',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '0',
   shiftValue: ')',
+  keycode: 'digit0',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '-',
   shiftValue: '_',
+  keycode: 'minus',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '=',
   shiftValue: '+',
+  keycode: 'equal',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'Backspace',
   shiftValue: 'Backspace',
+  keycode: 'backspace',
   class: 'keyboard__btn keyboard__btn_comand_big',
 }], [{
   value: 'Tab',
   shiftValue: 'Tab',
+  keycode: 'tab',
   class: 'keyboard__btn keyboard__btn_comand_small',
 }, {
   value: 'q',
   shiftValue: 'Q',
+  keycode: 'keyq',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'w',
   shiftValue: 'W',
+  keycode: 'keyw',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'e',
   shiftValue: 'E',
+  keycode: 'keye',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'r',
   shiftValue: 'R',
+  keycode: 'keyr',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 't',
   shiftValue: 'T',
+  keycode: 'keyt',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'y',
   shiftValue: 'Y',
+  keycode: 'keyy',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'u',
   shiftValue: 'U',
+  keycode: 'keyu',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'i',
   shiftValue: 'I',
+  keycode: 'keyi',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'o',
   shiftValue: 'O',
+  keycode: 'keyo',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'p',
   shiftValue: 'P',
+  keycode: 'keyp',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '[',
   shiftValue: '{',
+  keycode: 'bracketleft',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: ']',
   shiftValue: '}',
+  keycode: 'bracketright',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '\\',
@@ -113,146 +140,182 @@ const en = [[{
 }, {
   value: 'Del',
   shiftValue: 'Del',
+  keycode: 'delete',
   class: 'keyboard__btn keyboard__btn_comand_small',
 }], [{
   value: 'CapsLock',
   shiftValue: 'CapsLock',
+  keycode: 'capslock',
   class: 'keyboard__btn keyboard__btn_comand_big',
 }, {
   value: 'a',
   shiftValue: 'A',
+  keycode: 'keya',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 's',
   shiftValue: 'S',
+  keycode: 'keys',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'd',
   shiftValue: 'D',
+  keycode: 'keyd',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'f',
   shiftValue: 'F',
+  keycode: 'keyf',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'g',
   shiftValue: 'G',
+  keycode: 'keyg',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'h',
   shiftValue: 'H',
+  keycode: 'keyh',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'j',
   shiftValue: 'J',
+  keycode: 'keyj',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'k',
   shiftValue: 'K',
+  keycode: 'keyk',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'l',
   shiftValue: 'L',
+  keycode: 'keyl',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: ';',
   shiftValue: ':',
+  keycode: 'semicolon',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '\'',
   shiftValue: '"',
+  keycode: 'quote',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'Enter',
   shiftValue: 'Enter',
+  keycode: 'enter',
   class: 'keyboard__btn keyboard__btn_comand_big',
 }], [{
   value: 'Shift',
   shiftValue: 'Shift',
+  keycode: 'shiftleft',
   class: 'keyboard__btn keyboard__btn_comand_big',
 }, {
   value: 'z',
   shiftValue: 'Z',
+  keycode: 'keyz',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'x',
   shiftValue: 'X',
+  keycode: 'keyx',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'c',
   shiftValue: 'C',
+  keycode: 'keyc',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'v',
   shiftValue: 'V',
+  keycode: 'keyv',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'b',
   shiftValue: 'B',
+  keycode: 'keyb',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'n',
   shiftValue: 'N',
+  keycode: 'keyn',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: 'm',
   shiftValue: 'M',
+  keycode: 'keym',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: ',',
   shiftValue: '<',
+  keycode: 'comma',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '.',
   shiftValue: '>',
+  keycode: 'period',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '/',
   shiftValue: '?',
+  keycode: 'slash',
   class: 'keyboard__btn keyboard__btn_symbol',
 }, {
   value: '↑',
   shiftValue: '↑',
+  keycode: 'arrowup',
   class: 'keyboard__btn keyboard__btn_comand_small',
 }, {
   value: 'Shift',
   shiftValue: 'Shift',
+  keycode: 'shiftright',
   class: 'keyboard__btn keyboard__btn_comand_big',
 }], [{
   value: 'Ctrl',
   shiftValue: 'Ctrl',
+  keycode: 'controlleft',
   class: 'keyboard__btn keyboard__btn_comand_small',
 }, {
   value: 'Win',
   shiftValue: 'Win',
+  keycode: 'metaleft',
   class: 'keyboard__btn keyboard__btn_comand_small',
 }, {
   value: 'Alt',
   shiftValue: 'Alt',
+  keycode: 'altleft',
   class: 'keyboard__btn keyboard__btn_comand_small',
 }, {
   value: ' ',
   shiftValue: ' ',
+  keycode: 'space',
   class: 'keyboard__btn keyboard__btn_space',
 }, {
   value: 'Alt',
   shiftValue: 'Alt',
+  keycode: 'altright',
   class: 'keyboard__btn keyboard__btn_comand_small',
 }, {
   value: '←',
   shiftValue: '←',
+  keycode: 'arrowleft',
   class: 'keyboard__btn keyboard__btn_comand_small',
 }, {
   value: '↓',
   shiftValue: '↓',
+  keycode: 'arrowdown',
   class: 'keyboard__btn keyboard__btn_comand_small',
 }, {
   value: '→',
   shiftValue: '→',
+  keycode: 'arrowright',
   class: 'keyboard__btn keyboard__btn_comand_small',
 }, {
   value: 'Ctrl',
   shiftValue: 'Ctrl',
+  keycode: 'controlright',
   class: 'keyboard__btn keyboard__btn_comand_small',
 }]];
 
@@ -315,6 +378,7 @@ const ru = [[{
 }], [{
   value: 'Tab',
   shiftValue: 'Tab',
+
   class: 'keyboard__btn keyboard__btn_comand_small',
 }, {
   value: 'й',
